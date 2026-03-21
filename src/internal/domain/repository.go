2918aeae7a7367ed7a38b2f283fb1b1c9eee3fc0
Владимир Елистратов <vladedev@MacBook-Pro-Vladimir.local>
2026-03-21 13:46:16 +0300

    ye[

--- src/internal/domain/repository.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-// Интерфейс, который сохраняет и находит игру по UUID
-type GameRepository interface {
-	Save(game *Game) error
-	Get(id string) (*Game, error)
-}
--- src/internal/domain/service.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-// Интерфейс, который определяет правила игры
-type GameService interface {
-	GetNextMove(board Board) (row, col int, err error) // Куда ходить (минимакс)
-	ValidateBoard(current, updated Board) error        // Корректность хода
-	IsGameOver(board Board) bool                       // Есть ли победитель или ничья
-}
